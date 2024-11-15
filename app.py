e655207fbc2c501db57b59d6cbb1d22d969d4da0
Mohd HUzaif <103990272+khanhuzaif348@users.noreply.github.com>
2024-11-15 09:45:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from PIL import Image
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_gemini_response(input_prompt,image):
-    model = genai.GenerativeModel('gemini-pro-vision')
+    model = genai.GenerativeModel('gemini-1.5-pro-vision-002')
     response = model.generate_content([input_prompt,image[0]])
     return response.text
 
